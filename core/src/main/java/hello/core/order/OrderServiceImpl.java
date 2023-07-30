5ec21b46b640ad86cb1e863765d264e6a11f5a09
DESKTOP-AP8E0B9\AAA <nalza1111@naver.com>
2023-07-30 19:46:26 +0900

    230730 스프링강의

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,31 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //할인정책 변경 => OrderServiceImpl.java 수정(DIP, OCP 위반)
+
+    //이렇게 해결하자 =>
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy  discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
