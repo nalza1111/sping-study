f6eb97b31d4330d87e1ac221122af511faa73b09
DESKTOP-AP8E0B9\AAA <nalza1111@naver.com>
2023-10-22 23:16:37 +0900

    [231022] 서블릿 -> jsp변경 // 뷰, 컨트롤러 분리

---
+++ springmvc/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFromServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFromServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFromServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        // 컨트롤로에서 뷰로 이동...서블릿에서 다른 서블릿이나 jsp 재호출 가능
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+        // => 오 이거 경로 http://localhost:8080/servlet-mvc/members/new-form 뜸! .jsp 안뜸~
+        //WEB-INF => 경로 직접쳐서 진입 불가능... 항상 컨트롤러를 타야 부를 수 있음
+
+    }
+}
